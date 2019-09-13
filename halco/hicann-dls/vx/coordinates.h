fa95a1112bd3d9dad32fbdbc51fe2c7b28df3d4b
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2019-09-13 18:16:31 +0200

    Split out synapse driver coordinates to separate file for HX
    Change-Id: I635013c0f8ae5dc621af2df82ddee268ac488864

--- halco/hicann-dls/vx/coordinates.h
+++ halco/hicann-dls/vx/coordinates.h
@@ -25,6 +25,7 @@ extern "C"
 #include "halco/hicann-dls/vx/reset.h"
 #include "halco/hicann-dls/vx/routing_crossbar.h"
 #include "halco/hicann-dls/vx/spi.h"
+#include "halco/hicann-dls/vx/synapse_driver.h"
 #include "halco/hicann-dls/vx/timing.h"
 #include "halco/hicann-dls/vx/xboard.h"
 
@@ -55,66 +56,6 @@ struct GENPYBIND(inline_base("*")) CADCConfigOnDLS
 	static const CADCConfigOnDLS top;
 	static const CADCConfigOnDLS bottom;
 };
-
-/*****************\
-   SynapseDriver
-\*****************/
-
-struct CommonSynapseDriverConfigOnDLS;
-
-struct GENPYBIND(inline_base("*")) SynapseDriverOnSynapseDriverBlock
-    : public common::detail::RantWrapper<SynapseDriverOnSynapseDriverBlock, uint_fast16_t, 127, 0>
-{
-	constexpr explicit SynapseDriverOnSynapseDriverBlock(uintmax_t const val = 0) GENPYBIND(implicit_conversion) :
-	    rant_t(val)
-	{}
-};
-
-struct GENPYBIND(inline_base("*")) SynapseDriverBlockOnDLS
-    : public common::detail::RantWrapper<SynapseDriverBlockOnDLS, uint_fast16_t, 1, 0>
-{
-	constexpr explicit SynapseDriverBlockOnDLS(uintmax_t const val = 0) GENPYBIND(implicit_conversion) :
-	    rant_t(val)
-	{}
-
-	CommonSynapseDriverConfigOnDLS toCommonSynapseDriverConfigOnDLS() const;
-
-	static const SynapseDriverBlockOnDLS top;
-	static const SynapseDriverBlockOnDLS bottom;
-};
-
-HALCO_COORDINATE_MIXIN(SynapseDriverMixin, SynapseDriverBlockOnDLS, synapse_driver)
-
-struct GENPYBIND(inline_base("*SynapseDriverMixin*")) SynapseDriverOnDLS
-    : public SynapseDriverMixin<SynapseDriverOnDLS, SynapseDriverOnSynapseDriverBlock>
-{
-	SynapseDriverOnDLS() = default;
-
-	explicit SynapseDriverOnDLS(
-	    SynapseDriverOnSynapseDriverBlock const& synapse_driver,
-	    SynapseDriverBlockOnDLS const& block = SynapseDriverBlockOnDLS()) :
-	    mixin_t(synapse_driver, block)
-	{}
-
-	explicit SynapseDriverOnDLS(enum_type const& e) : mixin_t(e) {}
-
-	SynapseDriverOnSynapseDriverBlock toSynapseDriverOnSynapseDriverBlock() const { return This(); }
-};
-
-struct GENPYBIND(inline_base("*")) CommonSynapseDriverConfigOnDLS
-    : public common::detail::RantWrapper<CommonSynapseDriverConfigOnDLS, uint_fast16_t, 1, 0>
-{
-	constexpr explicit CommonSynapseDriverConfigOnDLS(uintmax_t const val = 0) : rant_t(val) {}
-
-	SynapseDriverBlockOnDLS toSynapseDriverBlockOnDLS() const
-	{
-		return SynapseDriverBlockOnDLS(toEnum());
-	}
-
-	static const CommonSynapseDriverConfigOnDLS top;
-	static const CommonSynapseDriverConfigOnDLS bottom;
-};
-
 
 /*****************\
    PADIBus
@@ -545,10 +486,6 @@ struct GENPYBIND(inline_base("*")) CADCSampleOnCADCSampleQuad
 namespace std {
 
 HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::CADCConfigOnDLS)
-HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::SynapseDriverOnSynapseDriverBlock)
-HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::SynapseDriverBlockOnDLS)
-HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::SynapseDriverOnDLS)
-HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::CommonSynapseDriverConfigOnDLS)
 HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::CommonSynramConfigOnDLS)
 HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::SynapseQuadColumnOnDLS)
 HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::SynapseRowOnSynram)
---
+++ halco/hicann-dls/vx/synapse_driver.h
@@ -0,0 +1,83 @@
+#pragma once
+#include <stdint.h>
+
+#include "halco/common/genpybind.h"
+#include "halco/common/geometry.h"
+#include "halco/common/mixin.h"
+
+namespace halco::hicann_dls::vx GENPYBIND_TAG_HALCO_HICANN_DLS_VX {
+
+/*****************\
+   SynapseDriver
+\*****************/
+
+struct GENPYBIND(inline_base("*")) SynapseDriverOnSynapseDriverBlock
+    : public common::detail::RantWrapper<SynapseDriverOnSynapseDriverBlock, uint_fast16_t, 127, 0>
+{
+	constexpr explicit SynapseDriverOnSynapseDriverBlock(uintmax_t const val = 0)
+	    GENPYBIND(implicit_conversion) :
+	    rant_t(val)
+	{}
+};
+
+
+struct CommonSynapseDriverConfigOnDLS;
+
+struct GENPYBIND(inline_base("*")) SynapseDriverBlockOnDLS
+    : public common::detail::RantWrapper<SynapseDriverBlockOnDLS, uint_fast16_t, 1, 0>
+{
+	constexpr explicit SynapseDriverBlockOnDLS(uintmax_t const val = 0)
+	    GENPYBIND(implicit_conversion) :
+	    rant_t(val)
+	{}
+
+	CommonSynapseDriverConfigOnDLS toCommonSynapseDriverConfigOnDLS() const;
+
+	static const SynapseDriverBlockOnDLS top;
+	static const SynapseDriverBlockOnDLS bottom;
+};
+
+
+HALCO_COORDINATE_MIXIN(SynapseDriverMixin, SynapseDriverBlockOnDLS, synapse_driver)
+
+struct GENPYBIND(inline_base("*SynapseDriverMixin*")) SynapseDriverOnDLS
+    : public SynapseDriverMixin<SynapseDriverOnDLS, SynapseDriverOnSynapseDriverBlock>
+{
+	SynapseDriverOnDLS() = default;
+
+	explicit SynapseDriverOnDLS(
+	    SynapseDriverOnSynapseDriverBlock const& synapse_driver,
+	    SynapseDriverBlockOnDLS const& block = SynapseDriverBlockOnDLS()) :
+	    mixin_t(synapse_driver, block)
+	{}
+
+	explicit SynapseDriverOnDLS(enum_type const& e) : mixin_t(e) {}
+
+	SynapseDriverOnSynapseDriverBlock toSynapseDriverOnSynapseDriverBlock() const { return This(); }
+};
+
+
+struct GENPYBIND(inline_base("*")) CommonSynapseDriverConfigOnDLS
+    : public common::detail::RantWrapper<CommonSynapseDriverConfigOnDLS, uint_fast16_t, 1, 0>
+{
+	constexpr explicit CommonSynapseDriverConfigOnDLS(uintmax_t const val = 0) : rant_t(val) {}
+
+	SynapseDriverBlockOnDLS toSynapseDriverBlockOnDLS() const
+	{
+		return SynapseDriverBlockOnDLS(toEnum());
+	}
+
+	static const CommonSynapseDriverConfigOnDLS top;
+	static const CommonSynapseDriverConfigOnDLS bottom;
+};
+
+} // namespace halco::hicann_dls::vx
+
+namespace std {
+
+HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::SynapseDriverOnSynapseDriverBlock)
+HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::SynapseDriverBlockOnDLS)
+HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::SynapseDriverOnDLS)
+HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::CommonSynapseDriverConfigOnDLS)
+
+} // namespace std
