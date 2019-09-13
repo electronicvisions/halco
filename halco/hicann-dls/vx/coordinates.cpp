9ca5059752bbd134a1c2cecb3a33854bc674844d
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2019-09-13 18:37:07 +0200

    Split out PADI coordinates to separate file for HX
    Change-Id: Ib818570e4a467b348761521b442ef55a18815fbd

--- halco/hicann-dls/vx/coordinates.cpp
+++ halco/hicann-dls/vx/coordinates.cpp
@@ -14,50 +14,6 @@ PPUOnDLS CADCConfigOnDLS::toPPUOnDLS() const
 	return PPUOnDLS(toEnum());
 }
 
-CommonSynapseDriverConfigOnDLS SynapseDriverBlockOnDLS::toCommonSynapseDriverConfigOnDLS() const
-{
-	return CommonSynapseDriverConfigOnDLS(toEnum());
-}
-
-SynapseDriverBlockOnDLS const SynapseDriverBlockOnDLS::top{enum_vertical_top};
-SynapseDriverBlockOnDLS const SynapseDriverBlockOnDLS::bottom{enum_vertical_bottom};
-
-CommonSynapseDriverConfigOnDLS const CommonSynapseDriverConfigOnDLS::top{enum_vertical_top};
-CommonSynapseDriverConfigOnDLS const CommonSynapseDriverConfigOnDLS::bottom{
-    enum_vertical_bottom};
-
-PADIBusBlockOnDLS const PADIBusBlockOnDLS::bottom{enum_vertical_bottom};
-PADIBusBlockOnDLS const PADIBusBlockOnDLS::top{enum_vertical_top};
-
-PADIEventOnDLS const PADIEventOnDLS::bottom{enum_vertical_bottom};
-PADIEventOnDLS const PADIEventOnDLS::top{enum_vertical_top};
-
-CommonPADIBusConfigOnDLS const CommonPADIBusConfigOnDLS::bottom{enum_vertical_bottom};
-CommonPADIBusConfigOnDLS const CommonPADIBusConfigOnDLS::top{enum_vertical_top};
-
-CommonSTPConfigOnDLS const CommonSTPConfigOnDLS::bottom{enum_vertical_bottom};
-CommonSTPConfigOnDLS const CommonSTPConfigOnDLS::top{enum_vertical_top};
-
-SynramOnDLS PADIBusBlockOnDLS::toSynramOnDLS() const
-{
-	return SynramOnDLS(toEnum());
-}
-
-PADIEventOnDLS PADIBusBlockOnDLS::toPADIEventOnDLS() const
-{
-	return PADIEventOnDLS(toEnum());
-}
-
-CommonPADIBusConfigOnDLS PADIBusBlockOnDLS::toCommonPADIBusConfigOnDLS() const
-{
-	return CommonPADIBusConfigOnDLS(toEnum());
-}
-
-CommonSTPConfigOnDLS PADIBusBlockOnDLS::toCommonSTPConfigOnDLS() const
-{
-	return CommonSTPConfigOnDLS(toEnum());
-}
-
 CADCChannelType const CADCChannelType::causal{0};
 CADCChannelType const CADCChannelType::acausal{1};
 
--- halco/hicann-dls/vx/coordinates.h
+++ halco/hicann-dls/vx/coordinates.h
@@ -20,6 +20,7 @@ extern "C"
 #include "halco/hicann-dls/vx/jtag.h"
 #include "halco/hicann-dls/vx/neuron.h"
 #include "halco/hicann-dls/vx/omnibus.h"
+#include "halco/hicann-dls/vx/padi.h"
 #include "halco/hicann-dls/vx/pll.h"
 #include "halco/hicann-dls/vx/ppu.h"
 #include "halco/hicann-dls/vx/reset.h"
@@ -42,9 +43,6 @@ namespace halco {
 namespace hicann_dls {
 namespace vx GENPYBIND_TAG_HALCO_HICANN_DLS_VX {
 
-struct CrossbarInputOnDLS;
-struct CrossbarL2OutputOnDLS;
-
 /**********\
     CADC
 \**********/
@@ -58,94 +56,6 @@ struct GENPYBIND(inline_base("*")) CADCConfigOnDLS
 
 	static const CADCConfigOnDLS top;
 	static const CADCConfigOnDLS bottom;
-};
-
-/*****************\
-   PADIBus
-\*****************/
-
-struct PADIEventOnDLS;
-struct CommonPADIBusConfigOnDLS;
-struct CommonSTPConfigOnDLS;
-
-struct GENPYBIND(inline_base("*")) PADIBusBlockOnDLS
-    : public common::detail::RantWrapper<PADIBusBlockOnDLS, uint_fast16_t, 1, 0>
-{
-	constexpr explicit PADIBusBlockOnDLS(uintmax_t const val = 0) GENPYBIND(implicit_conversion) :
-	    rant_t(val)
-	{}
-
-	SynramOnDLS toSynramOnDLS() const;
-	PADIEventOnDLS toPADIEventOnDLS() const;
-	CommonPADIBusConfigOnDLS toCommonPADIBusConfigOnDLS() const;
-	CommonSTPConfigOnDLS toCommonSTPConfigOnDLS() const;
-
-	static const PADIBusBlockOnDLS top;
-	static const PADIBusBlockOnDLS bottom;
-};
-
-struct GENPYBIND(inline_base("*")) PADIBusOnPADIBusBlock
-    : public common::detail::RantWrapper<PADIBusOnPADIBusBlock, uint_fast16_t, 3, 0>
-{
-	constexpr explicit PADIBusOnPADIBusBlock(uintmax_t const val = 0) GENPYBIND(implicit_conversion) :
-	    rant_t(val)
-	{}
-};
-
-
-HALCO_COORDINATE_MIXIN(PADIBusMixin, PADIBusBlockOnDLS, padi_bus)
-
-struct GENPYBIND(inline_base("*PADIBusMixin*")) PADIBusOnDLS
-    : public PADIBusMixin<PADIBusOnDLS, PADIBusOnPADIBusBlock>
-{
-	PADIBusOnDLS() = default;
-
-	explicit PADIBusOnDLS(
-	    PADIBusOnPADIBusBlock const& synapse_driver,
-	    PADIBusBlockOnDLS const& block = PADIBusBlockOnDLS()) :
-	    mixin_t(synapse_driver, block)
-	{}
-
-	explicit PADIBusOnDLS(enum_type const& e) : mixin_t(e) {}
-
-	PADIBusOnPADIBusBlock toPADIBusOnPADIBusBlock() const { return This(); }
-	CrossbarOutputOnDLS toCrossbarOutputOnDLS() const { return CrossbarOutputOnDLS(toEnum()); }
-};
-
-struct GENPYBIND(inline_base("*")) CommonPADIBusConfigOnDLS
-    : public common::detail::RantWrapper<CommonPADIBusConfigOnDLS, uint_fast16_t, 1, 0>
-{
-	constexpr explicit CommonPADIBusConfigOnDLS(uintmax_t const val = 0) : rant_t(val) {}
-
-	PADIBusBlockOnDLS toPADIBusBlockOnDLS() const { return PADIBusBlockOnDLS(toEnum()); }
-
-	static const CommonPADIBusConfigOnDLS top;
-	static const CommonPADIBusConfigOnDLS bottom;
-};
-
-struct GENPYBIND(inline_base("*")) CommonSTPConfigOnDLS
-    : public common::detail::RantWrapper<CommonSTPConfigOnDLS, uint_fast16_t, 1, 0>
-{
-	constexpr explicit CommonSTPConfigOnDLS(uintmax_t const val = 0) : rant_t(val) {}
-
-	PADIBusBlockOnDLS toPADIBusBlockOnDLS() const { return PADIBusBlockOnDLS(toEnum()); }
-
-	static const CommonSTPConfigOnDLS top;
-	static const CommonSTPConfigOnDLS bottom;
-};
-
-struct GENPYBIND(inline_base("*")) PADIEventOnDLS
-    : public common::detail::RantWrapper<PADIEventOnDLS, uint_fast16_t, 1, 0>
-{
-	constexpr explicit PADIEventOnDLS(uintmax_t const val = 0)
-	    GENPYBIND(implicit_conversion) :
-	    rant_t(val)
-	{}
-
-	PADIBusBlockOnDLS toPADIBusBlockOnDLS() const { return PADIBusBlockOnDLS(toEnum()); }
-
-	static const PADIEventOnDLS top;
-	static const PADIEventOnDLS bottom;
 };
 
 /******\
@@ -273,12 +183,6 @@ struct GENPYBIND(inline_base("*")) CADCSampleOnCADCSampleQuad
 namespace std {
 
 HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::CADCConfigOnDLS)
-HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::PADIBusOnDLS)
-HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::PADIBusBlockOnDLS)
-HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::PADIBusOnPADIBusBlock)
-HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::PADIEventOnDLS)
-HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::CommonPADIBusConfigOnDLS)
-HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::CommonSTPConfigOnDLS)
 HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::CADCChannelType)
 HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::CADCReadoutType)
 HALCO_GEOMETRY_HASH_CLASS(halco::hicann_dls::vx::CADCSampleQuadOnSynram)
