f50839aa39ed180e7f7e5d9ea9b015e4affe7d65
Christian Mauch <cmauch@kip.uni-heidelberg.de>
2019-11-11 14:07:05 +0100

    Remove outdated info in Wafer coordinate docstring
    Change-Id: I354fb9fcd01b8bb63eca772ec5c754494d1b491e

--- include/halco/hicann/v2/wafer.h
+++ include/halco/hicann/v2/wafer.h
@@ -13,8 +13,6 @@ namespace hicann {
 namespace v2 {
 
 /// Wafer coordinate
-/// The Wafer coordinate is also used to determine the FPGA type. We define
-/// old (Virtex) wafers/vsetups are using a value <= 2.
 struct Wafer : public common::detail::BaseType<Wafer, size_t> {
 	PYPP_CONSTEXPR explicit Wafer(value_type val) : base_t(val) {}
 	PYPP_CONSTEXPR explicit Wafer() : base_t() {}
