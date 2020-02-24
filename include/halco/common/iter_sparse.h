5d2c2e688bf4e6e926dedeb68c9967e882324efd
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2020-02-24 20:40:56 +0100

    Fix iter_sparse for small ranged types
    Change-Id: I7e655aaa7ef75fda90859f1388fc601d21eb3ca7

--- include/halco/common/iter_sparse.h
+++ include/halco/common/iter_sparse.h
@@ -11,6 +11,8 @@ namespace halco::common {
 /**
  * Iterate over iterable type with specified amount of samples to draw with uniform probability from
  * range while guaranteeing order.
+ * If the range size is smaller equal than the number of requested samples, all instances in the
+ * range will be returned in order.
  * @tparam T Type to iter over
  * @param num Number of samples to draw, first and last are always added, defaults to all
  */
@@ -25,12 +27,12 @@ std::vector<T> iter_sparse(std::optional<size_t> const num = std::nullopt)
 	if (!num) {
 		return all;
 	}
-	if (*num < (T::size != 1 ? 2 : 1)) {
+	if (all.size() <= 2) {
+		return all;
+	}
+	if (*num < 2) {
 		throw std::runtime_error(
 		    "iter_sparse always adds first and last in range, provided num is too small.");
-	}
-	if (T::size == 1) {
-		return all;
 	}
 	std::vector<T> ret;
 	ret.push_back(all.front());
