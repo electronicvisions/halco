ef845ccd9c06e06058e442b3d15a3cae6d0353ce
Eric Müller <mueller@kip.uni-heidelberg.de>
2021-07-13 16:17:42 +0200

    build(wafer): Remove genpybind and dep. targets
    It wasn't used, and it would require pinning genpybind in the wafer app…
    
    Depends-On: 15135
    In-Container: /containers/testing/c14803p6_2021-07-10_1.img
    Change-Id: Ife5763a67315a63fa20b54a932c7b6f47b5af965

--- include/halco/common/genpybind.h
+++ include/halco/common/genpybind.h
@@ -1,5 +1,16 @@
 #pragma once
+
+#if defined(__has_include) && !defined(PYPLUSPLUS)
+#if __has_include(<genpybind.h>)
 #include <genpybind.h>
+#else
+#define GENPYBIND(...)
+#define GENPYBIND_MANUAL(...)
+#endif
+#else
+#define GENPYBIND(...)
+#define GENPYBIND_MANUAL(...)
+#endif
 
 #define GENPYBIND_TAG_HALCO_COMMON GENPYBIND(tag(common))
 #define GENPYBIND_TAG_HALCO_HICANN_V2 GENPYBIND(tag(hicann_v2))
