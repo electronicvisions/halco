f1c1138641c1b5df4b0b743ed65a57e882db16b4
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2021-09-09 11:59:13 +0200

    Reduce built serialization archives to JSON and PortableBinary
    Depends-On: 15488
    Change-Id: Ide541f0d3173e2375b380dc02949b4903a0dac79

--- src/halco/hicann-dls/vx/v3/neuron.cpp
+++ src/halco/hicann-dls/vx/v3/neuron.cpp
@@ -1,10 +1,8 @@
 #include "halco/hicann-dls/vx/v3/neuron.h"
 
 #include "halco/common/cerealization_geometry.h"
-#include <cereal/archives/binary.hpp>
 #include <cereal/archives/json.hpp>
 #include <cereal/archives/portable_binary.hpp>
-#include <cereal/archives/xml.hpp>
 #include <cereal/cereal.hpp>
 #include <cereal/types/map.hpp>
 #include <cereal/types/vector.hpp>
@@ -15,21 +13,13 @@ namespace halco::hicann_dls::vx::v3 {
 
 #define EXPLICIT_INSTANTIATE_CEREAL_SERIALIZE(CLASS_NAME)                                          \
 	template void CLASS_NAME ::CEREAL_SERIALIZE_FUNCTION_NAME(                                     \
-	    cereal::BinaryOutputArchive&, std::uint32_t const);                                        \
-	template void CLASS_NAME ::CEREAL_SERIALIZE_FUNCTION_NAME(                                     \
-	    cereal::BinaryInputArchive&, std::uint32_t const);                                         \
-	template void CLASS_NAME ::CEREAL_SERIALIZE_FUNCTION_NAME(                                     \
 	    cereal::JSONOutputArchive&, std::uint32_t const);                                          \
 	template void CLASS_NAME ::CEREAL_SERIALIZE_FUNCTION_NAME(                                     \
 	    cereal::JSONInputArchive&, std::uint32_t const);                                           \
 	template void CLASS_NAME ::CEREAL_SERIALIZE_FUNCTION_NAME(                                     \
 	    cereal::PortableBinaryOutputArchive&, std::uint32_t const);                                \
 	template void CLASS_NAME ::CEREAL_SERIALIZE_FUNCTION_NAME(                                     \
-	    cereal::PortableBinaryInputArchive&, std::uint32_t const);                                 \
-	template void CLASS_NAME ::CEREAL_SERIALIZE_FUNCTION_NAME(                                     \
-	    cereal::XMLOutputArchive&, std::uint32_t const);                                           \
-	template void CLASS_NAME ::CEREAL_SERIALIZE_FUNCTION_NAME(                                     \
-	    cereal::XMLInputArchive&, std::uint32_t const);
+	    cereal::PortableBinaryInputArchive&, std::uint32_t const);
 
 EXPLICIT_INSTANTIATE_CEREAL_SERIALIZE(LogicalNeuronCompartments)
 EXPLICIT_INSTANTIATE_CEREAL_SERIALIZE(LogicalNeuronOnDLS)
